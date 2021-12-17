20636afbb52de291c3d8d675cf8a952be08a5d8b
GilbertoChinco <chinco.gilberto@gmail.com>
2021-12-17 15:53:44 -0600

    POO 1

--- POO/src/main/java/Clases_Objetos/Main.java
+++ POO/src/main/java/Clases_Objetos/Main.java
@@ -1,16 +1,21 @@
 
 package Clases_Objetos;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int n1, n2;
+        System.out.println("Digit the first  number:");
+        n1 = input.nextInt();
+        System.out.println("Digit the second  number:");
+        n2 = input.nextInt();
+        
         Operation op = new Operation();
-        
-        op.Read_numbers();
-        op.sumar();
-        op.restar();
-        op.dividir();
-        op.multiplicar();
-        
+        op.sumar(n1, n2);
+        op.restar(n1, n2);
+        op.dividir(n1, n2);
+        op.multiplicar(n1, n2); 
         op.Resultados();
     } 
 }
--- POO/src/main/java/Clases_Objetos/Operation.java
+++ POO/src/main/java/Clases_Objetos/Operation.java
@@ -1,41 +1,29 @@
 
 package Clases_Objetos;
-import java.util.Scanner;
 public class Operation {
     //Attributes
-    int number_1;
-    int number_2;
     int sum;
     int diff;
     int frac;
     int multiply;
     
-    Scanner input = new Scanner(System.in);
     //Method
     //Ask for numbers
-    public void Read_numbers(){
-        System.out.println("Digit the first number");
-        number_1 = input.nextInt();
-        
-        System.out.println("Digit the second number");
-        number_2 = input.nextInt();
-        
-    }
     
-    public void sumar(){
+    public void sumar(int number_1, int number_2){
         sum = number_1 + number_2;
     }
     
-    public void restar(){
+    public void restar(int number_1, int number_2){
         diff = number_1 - number_2;
     }
     
-    public void multiplicar(){
+    public void multiplicar(int number_1, int number_2){
         multiply = number_1 * number_2;
     }
     
-    public void dividir(){
-        frac = number_1 / number_2;
+    public void dividir(int number_1, int number_2){
+      frac = number_1 / number_2;
     }
     
     public void Resultados(){
