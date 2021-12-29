dde044dd970a378b941affe685acf6367db7492a
GilbertoChinco <chinco.gilberto@gmail.com>
2021-12-29 17:33:21 -0600

    GUI buttons

--- GUI/src/ventana/Ventana.java
+++ GUI/src/ventana/Ventana.java
@@ -3,12 +3,15 @@ package ventana;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 public class Ventana extends JFrame{
-    
+    public JPanel panel;
     public Ventana(){
         //(width, height) - Tamaño de la ventana
         this.setSize(500, 500);
@@ -24,9 +27,19 @@ public class Ventana extends JFrame{
     }
     
     private void iniciarComponentes(){
-        JPanel panel = new JPanel();  //Create a panel
+        colocarPaneles();
+        //colocarEtiquetas();
+        //colocarBotones();
+        colocarRadioBotones();
+    }
+    
+    private void colocarPaneles(){
+        panel = new JPanel();  //Create a panel
         panel.setLayout(null);
         this.getContentPane().add(panel);
+    }
+    
+    private void colocarEtiquetas(){
         JLabel etiqueta = new JLabel(); //Create a label
         etiqueta.setBounds(100, 100, 100, 100);
         etiqueta.setForeground(Color.BLUE); //Cambiar color de las letras
@@ -34,5 +47,28 @@ public class Ventana extends JFrame{
         etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
         panel.add(etiqueta);
         etiqueta.setFont(new Font("arial", Font.PLAIN, 20));
+    }
+    
+    private void colocarBotones(){
+        JButton boton1 = new JButton();
+        boton1.setBounds(100, 100, 100, 40);
+        boton1.setText("Aceptar");
+        boton1.setEnabled(true); //Interacion del boton
+        boton1.setMnemonic('a'); //Establecemos alt + letra
+        panel.add(boton1);
+    }
+    
+    private void colocarRadioBotones(){
+        JRadioButton radioboton1 = new JRadioButton("Opcion 1", true);
+        radioboton1.setBounds(50, 100, 100, 50);
+        panel.add(radioboton1);
+         
+        JRadioButton radioboton2 = new JRadioButton("Opcion 2", false);
+        radioboton2.setBounds(50, 150, 100, 50);
+        panel.add(radioboton2);
+        
+        ButtonGroup grupoRadioBotones = new ButtonGroup();
+        grupoRadioBotones.add(radioboton1);
+        grupoRadioBotones.add(radioboton2);
     }
 }
